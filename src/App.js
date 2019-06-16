a3d912b194904fd6db3d56fa52cc527d6590d538
Rimil Dey <rimil@springboard.com>
2019-06-16 13:06:15 +0530

    firebase: added firebase using npm and added support to store images to storage and urls to firestore

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Gallery from './screen-components/gallery/gallery';
 
+
 function App() {
   return (
     <div className="App">
--- src/screen-components/gallery/gallery.js
+++ src/screen-components/gallery/gallery.js
@@ -8,20 +8,77 @@ import karly from '../../dummy-data/images/karly-gomez-1561494-unsplash.jpg';
 import simone from '../../dummy-data/images/simone-hutsch-1678080-unsplash.jpg';
 import ursula from '../../dummy-data/images/ursula-lauriston-1673373-unsplash.jpg';
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
 class Gallery extends Component {
 
   photos = [cure, david, harley, karly, simone, ursula, cure, david, harley, karly, simone, ursula];
 
+  constructor(props) {
+    super(props);
+    const firebaseConfig = {
+      apiKey: "AIzaSyCZySZxPITGy8CzxywIxXGIh7MP8GL5E8c",
+      authDomain: "admin-dashboard-ceacb.firebaseapp.com",
+      databaseURL: "https://admin-dashboard-ceacb.firebaseio.com",
+      projectId: "admin-dashboard-ceacb",
+      storageBucket: "admin-dashboard-ceacb.appspot.com",
+      messagingSenderId: "34050641425",
+      appId: "1:34050641425:web:38b650461527084e"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  getImage(event) {
+    event.preventDefault();
+    let file = event.target.files[0];
+    this.saveImageMessage(file);
+  }
+
+  getUserName() {
+    return firebase.auth().currentUser.displayName;
+  }
+
+  saveImageMessage(file) {
+    firebase.storage().ref('images/' + file.name).put(file).then((snapshot) => {
+      return snapshot.ref.getDownloadURL().then((url) => {
+        let db = firebase.firestore();
+        let dbRef = db.collection("gallery").doc(file.name);
+        dbRef.set({
+          downloadURL: url
+        }).then( () => {
+          console.log("Data stored in Firestore!", url);
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+
+  }
+
   render() {
     return(
-      <div className='gallery-component'>
-        {
-          this.photos.map((photo, index) => {
-            return(
-              <Photo photoURL={photo} key={index}/>
-            )
-          })
-        }
+      <div className='gallery-upload-button-wrapper'>
+        <div className='gallery-component'>
+          {
+            this.photos.map((photo, index) => {
+              return(
+                <Photo photoURL={photo} key={index} />
+              )
+            })
+          }
+        </div>
+        <input
+          name="mediaCapture"
+          id="mediaCapture"
+          type="file"
+          accept="image/*"
+          capture="camera"
+          className='upload-input'
+          onChange={this.getImage.bind(this)}
+        />
+        <label htmlFor="mediaCapture">Upload Image</label>
       </div>
     )
   }
