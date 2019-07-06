330630096f0be25a75c1db3e96b410c506e592cf
Rimil Dey <rimil@springboard.com>
2019-07-06 16:13:20 +0530

    sidebar component: basic sidebar component made
    Made the basic skeleton of the sidebar component, with selection state and changes in UI when a particular button is selected

---
+++ src/screen-components/sidebar/sidebar.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import './sidebar.css';
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedID: 'gallery',
+    }
+  }
+
+  changeSelection(event, id) {
+    this.setState({
+      selectedID: id
+    });
+  }
+
+  applyActiveOrInactiveColourClass(id) {
+    return this.state.selectedID === id ? 'active-colour' : 'inactive-colour';
+  }
+
+  showOrHideActiveLineDiv(id) {
+    return this.state.selectedID === id ? 'show-active-line' : 'hide-active-line';
+  }
+
+  render() {
+    return (
+      <div className='sidebar-wrapper'>
+        <div
+          className='svg-button'
+          id='gallery'
+          onClick={(e) => this.changeSelection(e, 'gallery')}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" className='svg-item'>
+            <path fill="none" d="M0 0h24v24H0V0z"/>
+            <path
+              className={this.applyActiveOrInactiveColourClass('gallery')}
+              d="M15.56 10.81l-2.35 3.02-1.56-1.88c-.2-.25-.58-.24-.78.01l-1.74 2.23c-.26.33-.02.81.39.81h8.98c.41 0 .65-.47.4-.8l-2.55-3.39c-.19-.26-.59-.26-.79 0zM2 5c-.55 0-1 .45-1 1v15c0 1.1.9 2 2 2h15c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1-.45-1-1V6c0-.55-.45-1-1-1zm19-4H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm-1 16H8c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h12c.55 0 1 .45 1 1v12c0 .55-.45 1-1 1z"
+            />
+          </svg>
+          <div className={"active-line " + this.showOrHideActiveLineDiv('gallery')} />
+        </div>
+
+        <div
+          className='svg-button'
+          id='news'
+          onClick={(e) => this.changeSelection(e, 'news')}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" className='svg-item'>
+            <path
+              className={this.applyActiveOrInactiveColourClass('news')}
+              d="M12 23c1.1 0 1.99-.89 1.99-1.99h-3.98c0 1.1.89 1.99 1.99 1.99zm8.29-4.71L19 17v-6c0-3.35-2.36-6.15-5.5-6.83V3c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v1.17C7.36 4.85 5 7.65 5 11v6l-1.29 1.29c-.63.63-.19 1.71.7 1.71h15.17c.9 0 1.34-1.08.71-1.71zM13 16h-2v-2h2v2zm0-5c0 .55-.45 1-1 1s-1-.45-1-1V9c0-.55.45-1 1-1s1 .45 1 1v2z"
+            />
+          </svg>
+          <div className={"active-line " + this.showOrHideActiveLineDiv('news')} />
+        </div>
+
+        <div
+          className='svg-button'
+          id='site-text'
+          onClick={(e) => this.changeSelection(e, 'site-text')}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" className='svg-item'>
+            <path fill="none" d="M0 0h24v24H0V0z"/>
+            <path
+              className={this.applyActiveOrInactiveColourClass('site-text')}
+              d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM8 19h12c.55 0 1-.45 1-1s-.45-1-1-1H8c-.55 0-1 .45-1 1s.45 1 1 1zm0-6h12c.55 0 1-.45 1-1s-.45-1-1-1H8c-.55 0-1 .45-1 1s.45 1 1 1zM7 6c0 .55.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1H8c-.55 0-1 .45-1 1z"
+            />
+          </svg>
+          <div className={"active-line " + this.showOrHideActiveLineDiv('site-text')} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
