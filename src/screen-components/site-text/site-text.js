d1556a2dbd91d2c30c3303b29d77ea40562f5d24
Rimil Dey <rimil@springboard.com>
2019-07-10 01:05:32 +0530

    site-text component: Added UI content to the site text component
    1. added npm package for material core styles
    2. added text fields, headings and styles
    3. added publish button
    4. changed upload svg colour to white

--- src/screen-components/site-text/site-text.js
+++ src/screen-components/site-text/site-text.js
@@ -1,12 +1,80 @@
 import React, {Component} from 'react';
 import './site-text.css';
+import TextField from '@material-ui/core/TextField';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+import upload from '../../images/upload.svg';
+
+const useStyles = theme => ({
+  textField: {
+    width: '95%',
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+  },
+});
+
 
 class SiteText extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { classes } = this.props;
     return(
-      <div>Site text!</div>
+      <div className='site-text-wrapper'>
+        <div className='site-text-container'>
+          <section className='section-wrapper'>
+            <h2 className='section-header'>FROM THE ADMINISTRATION'S DESK</h2>
+            <ThemeProvider theme={theme}>
+              <TextField
+                id="outlined-textarea"
+                label="Principal's Message"
+                multiline
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+              />
+            </ThemeProvider>
+
+            <ThemeProvider theme={theme}>
+              <TextField
+                id="outlined-textarea"
+                label="Vice Principal's Message"
+                multiline
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+              />
+            </ThemeProvider>
+          </section>
+
+          <section className='section-wrapper'>
+            <ThemeProvider theme={theme}>
+              <h2 className='section-header'>WORDS FROM OUR FOUNDER</h2>
+              <TextField
+                id="outlined-textarea"
+                label="Founder's Message"
+                multiline
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+              />
+            </ThemeProvider>
+          </section>
+
+          <button className='upload-button'>
+            <img src={upload} className='upload-button-image'/>
+            PUBLISH NOW
+          </button>
+        </div>
+      </div>
     )
   }
 }
 
-export default SiteText;
+export default withStyles(useStyles())(SiteText);
